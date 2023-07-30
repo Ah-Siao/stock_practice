a382e3fb649d0cf889b9557989971b89a6796b59
Ah-Siao <vul09832042@gmail.com>
2023-07-30 21:39:20 +0800

    slightly modified

---
+++ custom_trans.py
@@ -0,0 +1,55 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from keras.layers import Flatten
+from keras import backend as K
+
+
+def transformer_encoder(inputs, head_size, num_heads, ff_dim, dropout=0):
+    x = layers.LayerNormalization(epsilon=1e-6)(inputs)
+    x = layers.MultiHeadAttention(
+        key_dim=head_size, num_heads=num_heads, dropout=dropout
+    )(x, x)
+    x = layers.Dropout(dropout)(x)
+    res = x+inputs
+    x = layers.LayerNormalization(epsilon=1e-6)(res)
+    x = layers.Conv1D(filters=ff_dim, kernel_size=1, activation='relu')(x)
+    x = layers.Dropout(dropout)(x)
+    x = layers.Conv1D(filters=inputs.shape[-1], kernel_size=1)(x)
+    return x+res
+
+
+def build_model(input_shape, head_size, num_heads, ff_dim, num_transformer_blocks, mlp_units, dropout=0, mlp_dropout=0):
+    inputs = keras.Input(shape=input_shape)
+    x = inputs
+    for _ in range(num_transformer_blocks):
+        x = transformer_encoder(x, head_size, num_heads, ff_dim, dropout)
+    x = layers.GlobalAveragePooling1D(data_format='channels_first')(x)
+    for dim in mlp_units:
+        x = layers.Dense(dim, activation='elu')(x)
+        x = layers.Dropout(mlp_dropout)(x)
+    outputs = layers.Dense(1, activation='linear')(x)
+    return keras.Model(inputs, outputs)
+
+
+def lr_scheduler(epoch, lr, warmup_epochs=30, decay_epochs=100, initial_lr=1e-6, base_lr=1e-3, min_lr=5e-5):
+    if epoch <= warmup_epochs:
+        pct = epoch / warmup_epochs
+        return ((base_lr - initial_lr) * pct) + initial_lr
+
+    if epoch > warmup_epochs and epoch < warmup_epochs+decay_epochs:
+        pct = 1 - ((epoch - warmup_epochs) / decay_epochs)
+        return ((base_lr - min_lr) * pct) + min_lr
+
+    return min_lr
+
+
+def shift(xs, n):
+    e = np.empty_like(xs)
+    if n >= 0:
+        e[:n] = np.nan
+        e[n:] = xs[:-n]
+    else:
+        e[n:] = np.nan
+        e[:n] = xs[-n:]
+    return e
--- pages/predict.py
+++ pages/predict.py
@@ -15,63 +15,11 @@ from tensorflow.keras import layers
 from keras.layers import Flatten
 from keras import backend as K
 import pickle
+from custom_trans import transformer_encoder, build_model, lr_scheduler, shift
 
 model_path = './model/'
 history_path = './history/'
 st.session_state['model'] = 0
-# st.text(os.path.exists(f'{model_path}8183model.pkl'))
-# st.text(os.path.exists(f'{history_path}8183 history.csv'))
-
-
-def transformer_encoder(inputs, head_size, num_heads, ff_dim, dropout=0):
-    x = layers.LayerNormalization(epsilon=1e-6)(inputs)
-    x = layers.MultiHeadAttention(
-        key_dim=head_size, num_heads=num_heads, dropout=dropout
-    )(x, x)
-    x = layers.Dropout(dropout)(x)
-    res = x+inputs
-    x = layers.LayerNormalization(epsilon=1e-6)(res)
-    x = layers.Conv1D(filters=ff_dim, kernel_size=1, activation='relu')(x)
-    x = layers.Dropout(dropout)(x)
-    x = layers.Conv1D(filters=inputs.shape[-1], kernel_size=1)(x)
-    return x+res
-
-
-def build_model(input_shape, head_size, num_heads, ff_dim, num_transformer_blocks, mlp_units, dropout=0, mlp_dropout=0):
-    inputs = keras.Input(shape=input_shape)
-    x = inputs
-    for _ in range(num_transformer_blocks):
-        x = transformer_encoder(x, head_size, num_heads, ff_dim, dropout)
-    x = layers.GlobalAveragePooling1D(data_format='channels_first')(x)
-    for dim in mlp_units:
-        x = layers.Dense(dim, activation='elu')(x)
-        x = layers.Dropout(mlp_dropout)(x)
-    outputs = layers.Dense(1, activation='linear')(x)
-    return keras.Model(inputs, outputs)
-
-
-def lr_scheduler(epoch, lr, warmup_epochs=30, decay_epochs=100, initial_lr=1e-6, base_lr=1e-3, min_lr=5e-5):
-    if epoch <= warmup_epochs:
-        pct = epoch / warmup_epochs
-        return ((base_lr - initial_lr) * pct) + initial_lr
-
-    if epoch > warmup_epochs and epoch < warmup_epochs+decay_epochs:
-        pct = 1 - ((epoch - warmup_epochs) / decay_epochs)
-        return ((base_lr - min_lr) * pct) + min_lr
-
-    return min_lr
-
-
-def shift(xs, n):
-    e = np.empty_like(xs)
-    if n >= 0:
-        e[:n] = np.nan
-        e[n:] = xs[:-n]
-    else:
-        e[n:] = np.nan
-        e[:n] = xs[-n:]
-    return e
-
 
 input_name = st.text_input('Please enter the stock ID')
 st.text("e.g. for 精星 8183.TWO, enter '8183.TWO'")
@@ -181,6 +129,7 @@ if st.session_state['model'] == 0:
 
 elif st.session_state['model'] == 1:
     st.text('Let us use our old model!')
+
 
 # load the model and history:
 df = pd.read_csv(f'history/{stock_num}history.csv')
